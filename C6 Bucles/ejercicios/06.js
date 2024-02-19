2e906b31db03c80478b532113a5231cc725c0bef
MATIAS NAHUEL NUBLE <matiasnuble@gmail.com>
2024-02-19 19:25:35 -0300

    Third commit

--- C6 Bucles/ejercicios/06.js
+++ C6 Bucles/ejercicios/06.js
@@ -2,6 +2,16 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+    // Convertir el número a cadena y obtener la longitud
+    let numStr = num.toString();
+    let longitud = numStr.length;
+  
+    // Verificar si la longitud es igual a 3
+    if (longitud === 3) {
+      return true;
+    } else {
+      return false;
+    }
 }
 
 module.exports = tieneTresDigitos;
